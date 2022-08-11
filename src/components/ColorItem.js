4040b3e3d9766473edee1603b74b5fa07764a46d
Juan Toro <jct20xx@gmail.com>
2022-08-11 17:31:46 -0400

    commit tree 6620acbedfa7962f529f400df01358b1e611b413

---
+++ src/components/ColorItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ColorItem(props) {
+    console.log(props)
+    return <li key={props.color} style={{ color: props.color }}>{props.color}</li>;
+  }
+
+  export default ColorItem;
